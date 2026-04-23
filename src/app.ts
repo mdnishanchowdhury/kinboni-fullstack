462225c43ca955f6942da4d9b58ca3c73c5b02d8
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2026-04-23 17:28:52 +0600

    feat: initial project setup and database schema design with prisma

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// Basic route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Express!');
+});
+
+export default app;
